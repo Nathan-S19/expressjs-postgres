e3bf557c8d3b37e2c238d06d893320c9744837d9
Nathan-S19 <nathan.sass@hotmail.com>
2022-12-14 20:57:06 +1000

    testing

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,26 @@ const pool = new pg.Pool();
 
 const app = express();
 const port = process.env.PORT || 3333;
+const router = express.Router();
 
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
+  const { rows } = await pool.query("SELECT * FROM formdata");
   res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
 });
+
+// router.post("/api/v1/submit", async (req, res) => {
+//   // send data to database
+//   try {
+//     await pool.
+//     res.status(200).send("Success");
+//   } catch (err) {
+//     res.status(400).send("Error");
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
